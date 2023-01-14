c991263228d224340975bb7f8ff8de1bcd5d7cfd
Haisam K. Ido <haisam.ido@nasa.gov>
2023-01-13 22:56:14 -0500

    expanding jins definitions and added maqam dictionary

--- maqamat.py
+++ maqamat.py
@@ -26,8 +26,8 @@ cents_in_octave = 1200
 TET_intervals = np.array([12, 24])
 cents_in_interval = cents_in_octave/TET_intervals
 
-print(nth_root_of_2(TET_intervals))
-print(cents_in_interval)
+# print(nth_root_of_2(TET_intervals))
+# print(cents_in_interval)
 
 tones = {
     "cents": {
@@ -86,11 +86,48 @@ jins = {
     "Must'ar": {}
 }
 
-maqam = {
-    "Bayat": {"tonic": "D4", "ajnas": [jins['Bayat']['cents'], jins['Nahwand']['cents']]}
+maqam_def = {
+    "Bayat": {"tonic": "D4", "ajnas": ["Bayat", "Nahwand"]} 
 }
 
-# print(jins)
-# print(jins['Ajam']['cents'])
+#print(maqam['Bayat']['ajnas'])
 
-print(maqam)
+i=0
+maqam_argv='Bayat'
+
+for key in maqam_def[maqam_argv]['ajnas']:
+    l=len(jins[key]['cents'])
+    if i == 0:
+        maqam = np.array(jins[key]['cents'])
+    else:
+        maqam = np.append(maqam,jins[key]['cents'][1:l])
+    i=i+1
+
+def cents_from_frequency(f1,f2,cents_in_octave):
+    return cents_in_octave*math.log2(f2 / f1)
+
+def frequency_from_cents(f1,cents,cents_in_octave):
+    return f1*math.exp(cents/cents_in_octave)
+
+#f2=frequency_from_cents(110,0,1200)
+
+#print(frequency_from_cents(293.665,200,1200))
+print(cents_from_frequency(293.665,329.628 ,1200))
+
+c=0
+cu=np.array([c])
+
+for key in maqam:
+    c=c+key
+    cu=np.append(cu,c)
+
+maqam_def[maqam_argv].update({ "cents":cu })
+
+# print(cu)
+
+#print(maqam_def[maqam_argv]['cents'])
+
+f1=293.665
+# E4 = 329.628 
+for cent in maqam_def[maqam_argv]['cents']:
+  print(cent,frequency_from_cents(f1,cent,cents_in_octave))
