b3b4c63e3485da13e7bee850f5b869789e218940
Haisam Ido <haisam.ido@nasa.gov>
2023-09-08 20:58:23 -0400

    added argvs

--- maqamat.py
+++ maqamat.py
@@ -3,6 +3,7 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 
 import wave
 import time
@@ -11,6 +12,36 @@ import sys
 import pyaudio
 
 from fractions import Fraction
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f0','--f0', type=float, default=440, help='foo help')
+parser.add_argument('-f1','--f1', type=float, default=220, help='foo help')
+parser.add_argument('-c','--cents-per-octave',  type=float, default=1200, help='cents per octave')
+parser.add_argument('-o','--number-of-octaves', type=float, default=1, help='number of octaves')
+
+parser.add_argument('-e','--by-et',        type=bool, default=True,  help='by equal temparement')
+parser.add_argument('-i','--et-intervals', type=float, default=12,  help='Number of equally tempered intervals')
+
+parser.add_argument('-R','--by-ratios', type=bool, default=False, help='providing scale by ratios')
+parser.add_argument('-r','--ratios',    type=str,  default=[1/1, 253/243, 16/15, 10/9 , 9/8, 32/27, 6/5, 5/4, 81/64, 4/3, 27/20, 45/32, 729/512, 3/2, 128/81, 8/5, 5/3, 27/16, 16/9, 9/5, 15/8, 243/128, 2/1], help='Scale by ratios')
+
+parser.add_argument('-l','--limit-denominator-factor', type=int, default=5, help='Limit denominator to calculate ratios')
+
+args = vars(parser.parse_args())
+
+f0                = args['f0']
+f1                = args['f1']
+cents_per_octave  = args['cents_per_octave']
+number_of_octaves = args['number_of_octaves']
+
+limit_denominator_factor = args['limit_denominator_factor']
+
+by_et              = args['by_et']
+et_intervals       = args['et_intervals']
+cents_per_interval = cents_per_octave/et_intervals
+
+by_ratios       = args['by_ratios']
+scale_by_ratios = np.array(args['ratios'])
 
 def nth_root_of_2(n):
     return (2)**(1/n)
@@ -33,7 +64,13 @@ def frequency_from_ratio(f1,f_ratio):
 
 def cents_from_ratio(f_ratio,cents_per_octave):
     return cents_per_octave*np.log2(f_ratio)
-    
+
+def generate_scale_by_et():
+    return 0
+
+def derive_scale_by_ratios():
+    return 0    
+        
 def generate_frequency(f):
     p = pyaudio.PyAudio()
 
@@ -68,44 +105,33 @@ def generate_frequency(f):
     stream.close()
     p.terminate()
 
-cents_per_octave   = 1200
-number_of_octaves  = 2
+    
+if by_et is True:
+    scale_by_cents       = np.arange(0, cents_per_octave+cents_per_interval, cents_per_interval)
+    scale_in_frequencies = frequency_from_cents(f1, scale_by_cents, cents_per_octave)
+    intervals            = scale_by_cents.size
+    limit_denominator    = intervals-1
 
-TET_intervals      = 53
-cents_per_interval = cents_per_octave/TET_intervals
+if by_ratios is True:
+    scale_by_cents       = cents_from_ratio(scale_by_ratios,cents_per_octave)
+    scale_in_frequencies = frequency_from_ratio(f1,scale_by_ratios)
+    intervals            = scale_by_cents.size
+    limit_denominator    = intervals*intervals
 
-f1                 = 220
+print(by_et)
+print(by_ratios)
 
-#scale_in_ratios = [256/243, 128/81, 32/27, 16/9, 4/3, 1/1, 3/2, 9/8, 27/16, 81/64, 243/128, 729/512 ]
-scale_in_ratios = np.array([1/1, 253/243, 16/15, 10/9 , 9/8, 32/27, 6/5, 5/4, 81/64, 4/3, 27/20, 45/32, 729/512, 3/2, 128/81, 8/5, 5/3, 27/16, 16/9, 9/5, 15/8, 243/128, 2/1])
-
-scale_in_cents  = np.arange(0, cents_per_octave+cents_per_interval, cents_per_interval)
-
-scale_in_cents      = cents_from_ratio(scale_in_ratios,cents_per_octave)
-
-delta_cents     = np.diff(scale_in_cents)
-
-scale_in_frequencies = frequency_from_cents(f1, scale_in_cents, cents_per_octave)
-#x=np.array(scale_in_ratios)/f1
-
-#print(x)
-scale_in_frequencies = frequency_from_ratio(f1,scale_in_ratios)
-#scale_in_cents      = cents_from_ratio(scale_in_frequencies,cents_per_octave)
-
-frequency_ratios     = scale_in_frequencies/f1
+frequency_ratios = scale_in_frequencies/f1
+delta_cents      = np.diff(scale_by_cents)
 
 print("%s  %9s  %10s  %8s  %7s  %9s  %11s  %11s" %("#", "cents", "f (Hz)", "f/f1", "ratio","fl(ratio)","abs err","rel err (%)"))
 print("--------------------------------------------------------------------------------")
 
-intervals = scale_in_cents.size
-
-for i, cent in enumerate(scale_in_cents):
+for i, cent in enumerate(scale_by_cents):
     f       = scale_in_frequencies[i]
     f_ratio = frequency_ratios[i]
-    
-#    delta_cent = delta_cents[i]
-    
-    fraction       = Fraction(f_ratio).limit_denominator(23*intervals)
+
+    fraction       = Fraction(f_ratio).limit_denominator(limit_denominator)
     fraction_float = float(fraction)
     
     aerror         = f_ratio - float(fraction)
