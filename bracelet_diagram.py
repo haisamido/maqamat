b9ebd1ab94a6446e2ecc3e7ac20a94ca59888962
Haisam Ido <haisam.ido@nasa.gov>
2024-02-11 16:51:23 -0500

    created functions instead

--- bracelet_diagram.py
+++ bracelet_diagram.py
@@ -5,48 +5,44 @@ import svgwrite
 import numpy as np
     
 # https://buildmedia.readthedocs.org/media/pdf/svgwrite/latest/svgwrite.pdf
-def main():
-    
-    canvas_width  =600
-    canvas_height =600
-    
-    dwg = svgwrite.Drawing('bracelet.svg', size=(canvas_width, canvas_height))
-    
-    #
-    pitches_per_octave   = 24
-    cents_per_octave     = 1200
-    intervals_per_octave = pitches_per_octave -1
-    
-    cents_per_interval = cents_per_octave/(pitches_per_octave)
-    stepSize           = (360/(pitches_per_octave))*(math.pi/180)
-    
-    i          =0
-    r_note     =15  # note radius 
-    r_bracelet =200 # radius of bracelet
 
-    cx = canvas_width/2
-    cy = canvas_height/2
-      
-    dwg.add(
-        dwg.circle( 
+cents_per_octave = 1200
+
+output_file   ="bracelet.svg"
+canvas_width  =600
+canvas_height =600
+
+r_note     =15  # note radius
+
+r_bracelet =200 # radius of bracelet
+cx = canvas_width/2
+cy = canvas_height/2
+
+cents = np.array([ 0.000, 111.731, 182.404, 315.641, 386.314, 498.045, 590.224, 609.776, 701.955, 813.686, 884.359, 1017.596, 1088.269])
+ 
+def create_canvas(output_file="bracelet.svg", canvas_width=600, canvas_height=600):
+    return svgwrite.Drawing(output_file, size=(canvas_width, canvas_height))
+    
+def add_bracelet_circle(obj='dwg', radius=200, cx=canvas_width/2, cy=canvas_height/2,stroke='white',stroke_width=1):
+    
+    obj.add(
+        obj.circle( 
             center=(cx,cy), 
-            r=r_bracelet, 
-            fill=svgwrite.rgb(140, 171, 255), 
+            r=radius, 
+            fill=svgwrite.rgb(140, 171, 255),
+            stroke=stroke, 
+            stroke_width=stroke_width,
             id='bracelet'
         )
     )
 
-    t = 0 #radians
+def add_notes_on_diagram(obj='dwg', cents=cents, r_bracelet=r_bracelet, cx=canvas_width/2, cy=canvas_height/2):
     
-    note={}
-    cents = np.array([ 0.000, 111.731, 182.404, 315.641, 386.314, 498.045, 590.224, 609.776, 701.955, 813.686, 884.359, 1017.596, 1088.269])
-#    cents = np.array([ 0.000, 300.0, 450, 600.0, 900.0])
-
+    theta =-1*math.pi/2
+    
     i=0
-    theta=-1*math.pi/2
-    
     for cent in cents:
-        step=(2*math.pi)*(cent/1200)
+        step=(2*math.pi)*(cent/cents_per_octave)
         
         x = r_bracelet* math.cos(step) + cx
         y = r_bracelet* math.sin(step) + cy
@@ -60,42 +56,38 @@ def main():
         x=xrot
         y=yrot
         
-        # x_text = 1.2*r_bracelet* math.cos(step) + cx
-        # y_text = 1.2*r_bracelet* math.sin(step) + cy
-        
-        dwg.add(
-            dwg.circle(
+        obj.add(
+            obj.circle(
                 center=(x,y),
                 r=r_note,
-                fill=svgwrite.rgb(0, 255, 0), 
-                stroke='green', 
-                stroke_width='2',
+                fill=svgwrite.rgb(0, 255, 0, '%'), 
+                stroke='red', 
+                stroke_width=1,
                 id=cent
             )
         )
 
-        dwg.add(
-            dwg.line(
+        obj.add(
+            obj.line(
                 (cx, cy), 
                 (x, y), 
                 stroke=svgwrite.rgb(10, 10, 16, '%'), 
                 stroke_width='.2'
             )
         )
-        
-        # dwg.add(
-        #     dwg.circle(
-        #         center=(x_text, y_text),
-        #         r=r_note,
-        #         fill=svgwrite.rgb(255, 0, 255, '%'),
-        #         stroke='black',
-        #         stroke_width='2'
-        #     )
-        # )
-        
+    
         i = i + 1
+    
+def add_cents_on_diagram():
+    print(0)
+  
+def main():
 
-    i=0
+    dwg = create_canvas()
+    
+    add_bracelet_circle(dwg)
+    add_notes_on_diagram(dwg)
+    
     dwg.save()
     
 if __name__ == "__main__":
